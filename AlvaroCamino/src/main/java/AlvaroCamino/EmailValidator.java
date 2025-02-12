54770f037ebe0ee733957504c063e4bdc7e59426
Alvaro Camino Martinez <alvarocamino05@gmail.com>
2025-02-12 09:55:37 +0100

    Añadido nuevo proyecto tdd maven

---
+++ AlvaroCamino/src/main/java/AlvaroCamino/EmailValidator.java
@@ -0,0 +1,7 @@
+package AlvaroCamino;
+
+public class EmailValidator {
+    public boolean isValid(String email) {
+        return email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$");
+    }
+}
